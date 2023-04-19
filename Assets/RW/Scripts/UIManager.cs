097deb7c594ef8bc94625e7f7829b0a299bb5fa5
Tiffany Ang <123000713+catc4ndy@users.noreply.github.com>
2023-04-19 15:20:01 -0400

    Adding Close Button

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-
 public class UIManager : MonoBehaviour
 {
-   public void StartGame() 
-{
-    SceneManager.LoadScene("RocketMouse");
-}
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator dialog;
 
+
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("RocketMouse");
+    }
+
+    public void OpenSettings() 
+    {
+    startButton.SetBool("isHidden", true);
+    settingsButton.SetBool("isHidden", true);
+        {
+        //..skipped..
+
+        dialog.SetBool("isHidden", false);
+        }
+    }
+
+    public void CloseSettings() 
+    {
+    startButton.SetBool("isHidden", false);
+    settingsButton.SetBool("isHidden", false);
+    dialog.SetBool("isHidden", true);
+    }
 }
